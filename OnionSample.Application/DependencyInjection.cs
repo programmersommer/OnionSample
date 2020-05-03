9708021f82f65c8bc04487e4afbe65f7875dbe8d
Alexej Sommer <asommer@yandex.ru>
2020-05-03 18:15:21 +0300

    DI related refactoring

---
+++ OnionSample.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OnionSample.Application.Interfaces;
+using OnionSample.Application.Services;
+using OnionSample.Application.UseCases;
+
+namespace OnionSample.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection RegisterApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAddToDoItemUseCase, AddToDoItemUseCase>();
+            services.AddScoped<ICalendarService, CalendarService>();
+
+            return services;
+        }
+    }
+}
---
+++ OnionSample.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OnionSample.Application.Interfaces;
+using OnionSample.Infrastructure.Persistence.Services;
+
+namespace OnionSample.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection RegisterInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IToDoItemPersistenceService, ToDoItemPersistenceService>();
+
+            return services;
+        }
+    }
+}
